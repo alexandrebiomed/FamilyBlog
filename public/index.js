a573db77c32520640bfe52857d66e1cec5cfbe6e
alexandrebiomed <65645417+alexandrebiomed@users.noreply.github.com>
2024-07-31 07:07:16 -0400

    Create a basic front-end for the HappyFamily Web App

---
+++ public/index.js
@@ -0,0 +1,27 @@
+
+$(".navigators div").hover(
+    function (){
+        $(this).addClass("navigators-hover");
+    },
+    function (){
+        $(this).removeClass("navigators-hover");
+    }
+)
+
+$(".login").hover(
+    function (){
+        $(this).addClass("login-hover");
+    },
+    function (){
+        $(this).removeClass("login-hover");
+    }
+)
+
+$(".signup").hover(
+    function (){
+        $(this).addClass("signup-hover");
+    },
+    function (){
+        $(this).removeClass("signup-hover");
+    }
+)
